4ed7e904d0dfde22d16ee8ce3ad0a09f4cec3795
xishku <gejianquan@139.com>
2022-12-03 14:33:03 +0800

    demo

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,61 +1,61 @@
-#include <iostream>
-#include <thread>
-#include <cstdint>
-#include <csignal>
-#include <ctime>
+// #include <iostream>
+// #include <thread>
+// #include <cstdint>
+// #include <csignal>
+// #include <ctime>
 #include <gtest/gtest.h>
 
 
-static volatile bool g_running = true;
+// static volatile bool g_running = true;
 
-int64_t TimeDiffInNs(struct timespec *pre, struct timespec *now)
-{
-    return (now->tv_sec - pre->tv_sec) * 1e9 + now->tv_nsec - pre->tv_nsec;
-}
+// int64_t TimeDiffInNs(struct timespec *pre, struct timespec *now)
+// {
+//     return (now->tv_sec - pre->tv_sec) * 1e9 + now->tv_nsec - pre->tv_nsec;
+// }
 
-void Loop()
-{
-    static const int PERIOD_IN_US = 1000;
-    static const int PERIOD_IN_US_2_NS = 1000;
-    static const int LOG_CIRCLE = 1000;
-    int64_t count = 0;
+// void Loop()
+// {
+//     static const int PERIOD_IN_US = 1000;
+//     static const int PERIOD_IN_US_2_NS = 1000;
+//     static const int LOG_CIRCLE = 1000;
+//     int64_t count = 0;
 
-    struct timespec init;
-    clock_gettime(CLOCK_MONOTONIC, &init);
+//     struct timespec init;
+//     clock_gettime(CLOCK_MONOTONIC, &init);
 
-    bool print = false;
-    while (true) {
-        if (!g_running) {
-            break;
-        }
+//     bool print = false;
+//     while (true) {
+//         if (!g_running) {
+//             break;
+//         }
 
-        ++count;
-        print = (count % LOG_CIRCLE == 0);
+//         ++count;
+//         print = (count % LOG_CIRCLE == 0);
 
-        struct timespec pre;
-        clock_gettime(CLOCK_MONOTONIC, &pre);
+//         struct timespec pre;
+//         clock_gettime(CLOCK_MONOTONIC, &pre);
 
-        struct timespec now;
-        clock_gettime(CLOCK_MONOTONIC, &now);
-        if (print) {
-            printf("count=%ld duration=%ldns\n", count, TimeDiffInNs(&pre, &now));
-        }
+//         struct timespec now;
+//         clock_gettime(CLOCK_MONOTONIC, &now);
+//         if (print) {
+//             printf("count=%ld duration=%ldns\n", count, TimeDiffInNs(&pre, &now));
+//         }
 
-        int64_t sleepTimeInUs = (count * PERIOD_IN_US * PERIOD_IN_US_2_NS - TimeDiffInNs(&init, &now))
-            / PERIOD_IN_US_2_NS;
-        if (sleepTimeInUs <= 0) {
-            printf("count=%ld time passed sleepTime=%ld\n", count, sleepTimeInUs);
-            continue;
-        }
+//         int64_t sleepTimeInUs = (count * PERIOD_IN_US * PERIOD_IN_US_2_NS - TimeDiffInNs(&init, &now))
+//             / PERIOD_IN_US_2_NS;
+//         if (sleepTimeInUs <= 0) {
+//             printf("count=%ld time passed sleepTime=%ld\n", count, sleepTimeInUs);
+//             continue;
+//         }
 
-        usleep(sleepTimeInUs);
-    }
-}
+//         usleep(sleepTimeInUs);
+//     }
+// }
 
-void Stop(int s)
-{
-    g_running = false;
-}
+// void Stop(int s)
+// {
+//     g_running = false;
+// }
 
 // int main()
 // {
@@ -69,8 +69,13 @@ void Stop(int s)
 //     return 0;
 // }
 
-TEST(s11, t1) {
-    EXPECT_EQ(1, 1);
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+TEST(sumtest, t1) {
+    EXPECT_EQ(3, Sum(1, 2));
 
 }
 
